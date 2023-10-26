6b356281f854ff006184c291d8725f1447e286bc
vadim <vadim.shevchenko@attractgroup.com>
2023-10-26 17:22:28 +0300

    Fix signals

--- django_facebook/signals.py
+++ django_facebook/signals.py
@@ -2,24 +2,24 @@ from django.dispatch import Signal
 
 
 # Sent right after user is created
-facebook_user_registered = Signal('user', 'facebook_data')
+facebook_user_registered = Signal()
 
 # Sent after user is created, before profile is updated with data from Facebook
-facebook_pre_update = Signal('user', 'profile', 'facebook_data')
-facebook_post_update = Signal('user', 'profile', 'facebook_data')
+facebook_pre_update = Signal()
+facebook_post_update = Signal()
 
 # Sent after storing the friends from graph to db
-facebook_post_store_friends = Signal('user', 'friends', 'current_friends', 'inserted_friends')
+facebook_post_store_friends = Signal()
 
 # Sent after storing the likes from graph to db
-facebook_post_store_likes = Signal('user', 'likes', 'current_likes', 'inserted_likes')
+facebook_post_store_likes = Signal()
 
 # Some signals for compatibility with Django Registration
 # A new user has registered.
-user_registered = Signal('user', 'request')
+user_registered = Signal()
 
 # A user has activated his or her account.
-user_activated = Signal('user', 'request')
+user_activated = Signal()
 
 # Run when the token extend finished
-facebook_token_extend_finished = Signal('user', 'profile', 'token_changed', 'old_token')
+facebook_token_extend_finished = Signal()
